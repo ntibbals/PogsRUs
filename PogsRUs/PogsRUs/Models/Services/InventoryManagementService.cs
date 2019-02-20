a9a2e59046009626baecf1436d227e249c0995e2
Jason Hiskey <jasonhi@crazyredhead.com>
2019-02-20 12:49:25 -0800

    Added skeleton methods from interface and added dep injection

---
+++ PogsRUs/PogsRUs/Models/Services/InventoryManagementService.cs
@@ -0,0 +1,45 @@
+﻿using PogsRUs.Data;
+using PogsRUs.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PogsRUs.Models.Services
+{
+    public class InventoryManagementService : IInventory
+    {
+        private readonly PogsRUsDbContext _context;
+
+        public InventoryManagementService(PogsRUsDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public Task CreateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
