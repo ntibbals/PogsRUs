cf109e68b6b3b86193e95bba3e83d75f030a81eb
ntibbals <ntibbals@gmail.com>
2019-02-20 13:01:04 -0800

    Account Controller constructor set

--- PogsRUs/PogsRUs/Controllers/AccountController.cs
+++ PogsRUs/PogsRUs/Controllers/AccountController.cs
@@ -2,12 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using PogsRUs.Models;
 
 namespace PogsRUs.Controllers
 {
     public class AccountController : Controller
     {
+        private UserManager<ApplicationUser> _userManager;
+
+        private SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
         public IActionResult Index()
         {
             return View();
