768da57fe85dba02c6e59e035b5f4c7b4e9bfd39
ntibbals <ntibbals@gmail.com>
2019-02-22 10:35:41 -0800

    Finished products controller w all CRUD functionality

--- PogsRUs/PogsRUs/Controllers/ProductController.cs
+++ PogsRUs/PogsRUs/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PogsRUs.Models;
 using PogsRUs.Models.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -45,6 +47,77 @@ namespace PogsRUs.Controllers
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID, Sku, Name, PogType, Price, Description, Image")] Product product)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    await _context.CreateProduct(product);
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(product);
+            }
+            catch (Exception)
+            {
+                return Redirect("https://http.cat/500");
+            }
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var product = await _context.GetProduct(id);
+
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID, Sku, Name, PogType, Price, Description, Image")] Product product)
+        {
+            if (id != product.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _context.UpdateProduct(product);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    throw;
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var product = await _context.GetProduct(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var product = await _context.GetProduct(id);
+            await _context.DeleteProduct(product);
+            return RedirectToAction(nameof(Index));
+        }
 
     }
 }
