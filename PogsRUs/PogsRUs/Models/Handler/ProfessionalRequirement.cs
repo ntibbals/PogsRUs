0a79bf8fb5cd64a2ecf9bd8e67f52116e3327fa6
Jason Hiskey <jasonhi@crazyredhead.com>
2019-02-22 09:10:31 -0800

    Framing out new Policy ProfessionalRequirement

---
+++ PogsRUs/PogsRUs/Models/Handler/ProfessionalRequirement.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PogsRUs.Models.Handler
+{
+    public class ProfessionalRequirement : AuthorizationHandler<ProfessionalRequirement>, IAuthorizationRequirement
+    {
+        private bool _isProfessional;
+
+        public ProfessionalRequirement(bool isProfessional)
+        {
+            _isProfessional = isProfessional;
+        }
+
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ProfessionalRequirement requirement)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
