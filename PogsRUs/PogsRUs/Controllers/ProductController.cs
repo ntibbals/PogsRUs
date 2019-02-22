1f22504fa452616f797a9d9b3ab6d7fa2b76b520
ntibbals <ntibbals@gmail.com>
2019-02-22 10:22:09 -0800

    built out index and details product controller methods

---
+++ PogsRUs/PogsRUs/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PogsRUs.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PogsRUs.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IInventory _context;
+
+        public ProductController(IInventory context)
+        {
+            _context = context;
+        }
+
+        /// Get Products
+        public async Task<IActionResult> Index(string searchString)
+        {
+            var product = await _context.GetProducts();
+            if(!String.IsNullOrEmpty(searchString))
+            {
+                product = product.Where(p => p.Name.Contains(searchString));
+            }
+
+            return View(product.ToList());
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var product = await _context.GetProduct(id);
+
+            if(product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+    }
+}
