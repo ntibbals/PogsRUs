23ffa9766e25e7b6ceac42d9fe6e8685f0d1db8f
ntibbals <ntibbals@gmail.com>
2019-02-26 12:17:09 -0800

    built out cart class

---
+++ PogsRUs/PogsRUs/Models/Cart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PogsRUs.Models
+{
+    public class Cart
+    {
+
+        public int UserID { get; set; }
+        public Dictionary<Product, int> CartProducts { get; set; }
+
+        public Cart(int userID)
+        {
+            UserID = userID;
+           Dictionary<Product, int> CartProducts = new Dictionary<Product, int>();
+        }
+    }
+}
