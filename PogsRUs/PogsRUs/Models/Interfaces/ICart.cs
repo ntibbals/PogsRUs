4c1cfcbc15934d3860c7477d0a0fe0765d86656d
Jason Hiskey <jasonhi@crazyredhead.com>
2019-02-26 12:29:33 -0800

    Added ICart interface.

---
+++ PogsRUs/PogsRUs/Models/Interfaces/ICart.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PogsRUs.Models.Interfaces
+{
+    interface ICart
+    {
+
+        /// <summary>
+        /// Create Cart
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <returns>Task</returns>
+        Task CreateCart(int userID);
+
+
+        /// <summary>
+        /// Retreives Cart that matches user ID
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <returns>Users cart</returns>
+        Task GetCart(int userID);
+
+        /// <summary>
+        /// Adds product and increased quanitiy of product by one
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Product product</returns>
+        Task<Product> AddProduct(Product product);
+
+        /// <summary>
+        /// Retreive all existing products in cart and returns as a list.
+        /// </summary>
+        /// <returns>List of Products</returns>
+        Task<IEnumerable<Product>> GetProducts();
+
+        /// <summary>
+        /// Deletes existing product from Cart.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        Task DeleteProduct(Product product);
+
+        /// <summary>
+        /// Deletes Cart.
+        /// </summary>
+        /// <param name="cart"></param>
+        /// <returns></returns>
+        Task DeleteCart(Cart cart);
+    }
+}
