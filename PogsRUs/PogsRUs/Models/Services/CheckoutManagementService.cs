777369bc0ed3e61ddd4831c07d7e7bcb94af833f
Jason Hiskey <jasonhi@crazyredhead.com>
2019-03-03 10:48:17 -0800

    Registered interface in Startup.CS

--- PogsRUs/PogsRUs/Models/Services/CheckoutManagementService.cs
+++ PogsRUs/PogsRUs/Models/Services/CheckoutManagementService.cs
@@ -1,4 +1,5 @@
 ﻿using PogsRUs.Data;
+using PogsRUs.Models.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace PogsRUs.Models.Services
 {
-    public class CheckoutManagementService
+    public class CheckoutManagementService : ICheckout
     {
         private readonly PogsRUsDbContext _context;
 
