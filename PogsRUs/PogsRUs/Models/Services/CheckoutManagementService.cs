a0da6249860bd8395656d7784e4c405d7244098c
Jason Hiskey <jasonhi@crazyredhead.com>
2019-03-03 10:46:21 -0800

    Registered TransactionHistory and TransactionHistory products into Data

---
+++ PogsRUs/PogsRUs/Models/Services/CheckoutManagementService.cs
@@ -0,0 +1,18 @@
+﻿using PogsRUs.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PogsRUs.Models.Services
+{
+    public class CheckoutManagementService
+    {
+        private readonly PogsRUsDbContext _context;
+
+        public CheckoutManagementService(PogsRUsDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
