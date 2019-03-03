51132b987ef81a0a2754ee97db2a942bc62538fc
Jason Hiskey <jasonhi@crazyredhead.com>
2019-03-03 10:25:47 -0800

    Built out TransactionHistory and TransactionHistoryProducts Classes

--- PogsRUs/PogsRUs/Models/TransactionHistory.cs
+++ PogsRUs/PogsRUs/Models/TransactionHistory.cs
@@ -7,5 +7,14 @@ namespace PogsRUs.Models
 {
     public class TransactionHistory
     {
+        public int ID { get; set; }
+        public string UserID { get; set; }
+        public IEnumerable<TransactionHistoryProduct> PurchasedProducts { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        public TransactionHistory(string userID)
+        {
+            UserID = userID;
+        }
     }
 }
---
+++ PogsRUs/PogsRUs/Models/TransactionHistoryProduct.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PogsRUs.Models
+{
+    public class TransactionHistoryProduct
+    {
+        public int ID { get; set; }
+        public int ProductID { get; set; }
+        public int CartID { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal SingleItemPrice { get; set; }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                return SingleItemPrice * Quantity;
+            }
+        }
+
+        public TransactionHistoryProduct(int productID, int cartID, string name, decimal price, int quantity)
+        {
+            ProductID = productID;
+            CartID = cartID;
+            Quantity = quantity;
+            Name = name;
+            SingleItemPrice = price;
+        }
+    }
+}
