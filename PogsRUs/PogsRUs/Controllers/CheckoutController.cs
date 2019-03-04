0ea2f387dbb571e78dbf2448bf07ce5e75a6bc31
ntibbals <ntibbals@gmail.com>
2019-03-04 13:20:19 -0800

    added cart partial to product page, updated order confrimation email to send out post clicking on checkout button

--- PogsRUs/PogsRUs/Controllers/CheckoutController.cs
+++ PogsRUs/PogsRUs/Controllers/CheckoutController.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using PogsRUs.Models;
 using PogsRUs.Models.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace PogsRUs.Controllers
@@ -11,15 +15,28 @@ namespace PogsRUs.Controllers
     {
 
         private readonly ICheckout _context;
+        private IEmailSender _emailSender;
+        private UserManager<ApplicationUser> _userManager;
 
-        public CheckoutController(ICheckout context)
+
+        public CheckoutController(ICheckout context, UserManager<ApplicationUser> userManager, IEmailSender emailSender)
         {
             _context = context;
+            _emailSender = emailSender;
         }
 
         public async Task<IActionResult> Summary(string userID)
         {
             var cart = await _context.CreateReceipt(userID);
+
+            //var ourUser = await _userManager.FindByEmailAsync(userID);
+            //string id = ourUser.Id;
+            StringBuilder stringBuilder = new StringBuilder();
+            //string userName = $"{ourUser.FirstName} {ourUser.LastName}";
+            stringBuilder.Append($"<p>Thank you for your order! Your total order amount was: {cart.TotalPrice}.");
+            stringBuilder.AppendLine("</p>");
+
+            await _emailSender.SendEmailAsync(userID, "", stringBuilder.ToString());
 
             return View(cart);
         }
