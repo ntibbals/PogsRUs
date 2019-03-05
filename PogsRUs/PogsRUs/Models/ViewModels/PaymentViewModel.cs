25199d89446d39aa6e29664fd62f505c7dbc6f1a
ntibbals <ntibbals@gmail.com>
2019-03-05 09:54:23 -0800

    built out payment view model

---
+++ PogsRUs/PogsRUs/Models/ViewModels/PaymentViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PogsRUs.Models.ViewModels
+{
+    public class PaymentViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Required]
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+        [Required]
+        [Display(Name = "Zip code")]
+        public string Zipcode { get; set; }
+
+        [Required]
+        [Display(Name = "Credit Card")]
+        [DataType(DataType.CreditCard)]
+        public string CreditCardNumber{ get; set; }
+
+
+        [Required]
+        [Display(Name = "Expiration Date")]
+        [DataType(DataType.Date)]
+        public string ExpirationDate { get; set; }
+    }
+}
