3c41d21a02fda81b199aecf2835bb944e626b2c5
ntibbals <ntibbals@gmail.com>
2019-03-05 10:08:21 -0800

    pulled in authorizenet nuget package, setup api base

---
+++ PogsRUs/PogsRUs/Models/Payment.cs
@@ -0,0 +1,30 @@
+﻿using AuthorizeNet.Api.Contracts.V1;
+using AuthorizeNet.Api.Controllers.Bases;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PogsRUs.Models
+{
+    public class Payment
+    {
+
+        private IConfiguration _configuration;
+
+        public Payment(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string Run()
+        {
+
+            ApiOperationBase<ANetApiRequest, ANetApiResponse>.RunEnvironment = AuthorizeNet.Environment.SANDBOX;
+
+            ApiOperationBase<ANetApiRequest, ANetApiResponse>.MerchantAuthentication = new merchantAuthenticationType();
+        }
+
+    }
+}
