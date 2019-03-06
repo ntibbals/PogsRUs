d27d7190197ade94ee0cd4fc3eae9334591b2568
Jason Hiskey <jasonhi@crazyredhead.com>
2019-03-06 09:57:23 -0800

    Completed OrderProductModel Tests

--- PogsRUs/ProductModelTestSuite/ModelTests/OrderProductModelTestSuite.cs
+++ PogsRUs/ProductModelTestSuite/ModelTests/OrderProductModelTestSuite.cs
@@ -52,85 +52,121 @@ namespace TestSuite.ModelTests
         [Fact]
         public void TestIDSet()
         {
-            
+            OrderProduct testOrderProduct = CreateOrderProduct();
+            testOrderProduct.ID = 100;
+
+            Assert.Equal(100, testOrderProduct.ID);
         }
 
         [Fact]
         public void TestIDGet()
         {
+            OrderProduct testOrderProduct = CreateOrderProduct();
 
+            Assert.Equal(1, testOrderProduct.ID);
         }
 
         [Fact]
         public void TestProductIDSet()
         {
+            OrderProduct testOrderProduct = CreateOrderProduct();
+            testOrderProduct.ProductID = 100;
 
+            Assert.Equal(100, testOrderProduct.ProductID);
         }
 
         [Fact]
         public void TestProductIDGet()
         {
+            OrderProduct testOrderProduct = CreateOrderProduct();
+            Product testProduct = CreateProduct();
 
+            Assert.Equal(testProduct.ID, testOrderProduct.ProductID);
         }
 
         [Fact]
         public void TestOrderIDSet()
         {
+            OrderProduct testOrderProduct = CreateOrderProduct();
+            testOrderProduct.OrderID = 100;
 
+            Assert.Equal(100, testOrderProduct.OrderID);
         }
 
         [Fact]
         public void TestOrderIDGet()
         {
+            OrderProduct testOrderProduct = CreateOrderProduct();
+            Order testOrder = CreateOrder();
 
+            Assert.Equal(testOrder.ID, testOrderProduct.OrderID);
         }
 
         [Fact]
         public void TestNameSet()
         {
+            OrderProduct testOrderProduct = CreateOrderProduct();
+            testOrderProduct.Name = "JimBob";
 
+            Assert.Equal("JimBob", testOrderProduct.Name);
         }
 
         [Fact]
         public void TestNameGet()
         {
+            OrderProduct testOrderProduct = CreateOrderProduct();
+            Product testProduct = CreateProduct();
 
+            Assert.Equal(testProduct.Name, testOrderProduct.Name);
         }
 
         [Fact]
         public void TestQuantitySet()
         {
+            OrderProduct testOrderProduct = CreateOrderProduct();
+            testOrderProduct.Quantity = 100;
 
+            Assert.Equal(100, testOrderProduct.Quantity);
         }
 
         [Fact]
         public void TestQuantityGet()
         {
-
+            OrderProduct testOrderProduct = CreateOrderProduct();
+            Assert.Equal(1, testOrderProduct.Quantity);
         }
 
         [Fact]
         public void TestSingleItemPriceSet()
         {
+            OrderProduct testOrderProduct = CreateOrderProduct();
+            testOrderProduct.SingleItemPrice = 100;
 
+            Assert.Equal(100, testOrderProduct.SingleItemPrice);
         }
 
         [Fact]
         public void TestSingleItemPriceGet()
         {
-
+            OrderProduct testOrderProduct = CreateOrderProduct();
+            Assert.Equal(1.00M, testOrderProduct.Quantity);
         }
 
         [Fact]
         public void TestTimeStampSet()
         {
+            OrderProduct testOrderProduct = CreateOrderProduct();
+            DateTime time = DateTime.Now;
+            testOrderProduct.TimeStamp = time;
 
+            Assert.Equal(time, testOrderProduct.TimeStamp);
         }
 
         [Fact]
         public void TestTimeStampGet()
         {
-
+            OrderProduct testOrderProduct = CreateOrderProduct();
+            Assert.Equal(DateTime.Today, testOrderProduct.TimeStamp);
         }
 
     }
