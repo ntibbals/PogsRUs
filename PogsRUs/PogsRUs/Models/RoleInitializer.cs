483f0e7752d7134a47b0bb89e118d5369688242c
ntibbals <ntibbals@gmail.com>
2019-03-05 20:17:11 -0800

    built out role intitializer and added roles to applicationsuer

---
+++ PogsRUs/PogsRUs/Models/RoleInitializer.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PogsRUs.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PogsRUs.Models
+{
+    public class RoleInitializer
+    {
+
+        /// <summary>
+        /// Setting up each specific application role with members and admin in initializer
+        /// </summary>
+        private static readonly List<IdentityRole> Roles = new List<IdentityRole>()
+        {
+            new IdentityRole{Name = ApplicationRoles.Member, NormalizedName = ApplicationRoles.Member.ToUpper(), ConcurrencyStamp = Guid.NewGuid().ToString() },
+
+            new IdentityRole{Name = ApplicationRoles.Admin, NormalizedName = ApplicationRoles.Admin.ToUpper(), ConcurrencyStamp = Guid.NewGuid().ToString() }
+        };
+
+        /// <summary>
+        /// Checking database for roles
+        /// </summary>
+        /// <param name="serviceProvider"></param>
+        public static void SeedData(IServiceProvider serviceProvider)
+        {
+
+            using (var dbContext = new ApplicationDbContext(serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+            {
+                dbContext.Database.EnsureCreated();
+                AddRoles(dbContext);
+            }
+
+        }
+
+        private static void AddRoles(ApplicationDbContext context)
+        {
+            if (context.Roles.Any()) return;
+
+            foreach (var role in Roles)
+            {
+                context.Roles.Add(role);
+                context.SaveChanges();
+            }
+        }
+    }
+}
