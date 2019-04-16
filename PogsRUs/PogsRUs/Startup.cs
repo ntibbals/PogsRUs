415f071f3269b4f95ab8a0f14d3cd4dc1dc42d22
ntibbals <ntibbals@gmail.com>
2019-04-16 09:21:02 -0700

    updated resources

--- PogsRUs/PogsRUs/Startup.cs
+++ PogsRUs/PogsRUs/Startup.cs
@@ -51,22 +51,22 @@ namespace PogsRUs
 
             /********************** DEFAULT **************************************/
 
-            //services.AddDbContext<ApplicationDbContext>(options =>
-            //options.UseSqlServer(Configuration["ConnectionStrings:IdentityDefaultConnection"]));
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration["ConnectionStrings:IdentityDefaultConnection"]));
 
-            //services.AddDbContext<PogsRUsDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
+            services.AddDbContext<PogsRUsDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
 
             /**********************Production***********************/
 
-            services.AddDbContext<PogsRUsDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:ProductionConnection"]));
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:IdentityProductionConnection"]));
+            //services.AddDbContext<PogsRUsDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:ProductionConnection"]));
+            //services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:IdentityProductionConnection"]));
 
-            //services.AddAuthentication()
-            //    .AddMicrosoftAccount(microsoftOptions =>
-            //    {
-            //        microsoftOptions.ClientId = Configuration["Authentication:Microsoft:ApplicationId"];
-            //        microsoftOptions.ClientSecret = Configuration["Authentication:Microsoft:Password"];
-            //    })
+            services.AddAuthentication()
+                .AddMicrosoftAccount(microsoftOptions =>
+                {
+                    microsoftOptions.ClientId = Configuration["Authentication:Microsoft:ApplicationId"];
+                    microsoftOptions.ClientSecret = Configuration["Authentication:Microsoft:Password"];
+                });
             //    .AddGoogle(googleOptions =>
             //    {
             //        googleOptions.ClientId = Configuration["Authentication:Google:ClientId"];
